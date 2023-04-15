6c4953229a839881e2bf686499b72fc7237692c7
giorgia-mineo <giomineo@gmail.com>
2023-04-15 10:04:01 +0100

    added basic functioning login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,44 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 
 const Login = () => {
+  const navigate = useNavigate()
+
+  const [ formFields, setFormFields ] = useState({
+    email: '',
+    password: '',
+  })
+
+  const handleChange = (e) => {
+    setFormFields({ ...formFields, [e.target.name]: [e.target.value] })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      formFields.email = formFields.email[0]
+      formFields.password = formFields.password[0]
+      console.log(formFields)
+      await axios.post('api/auth/login/', formFields)
+      navigate('/')
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
-    <div>login</div>
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <label htmlFor="email"></label>
+        <input type="text" name="email" placeholder="email" onChange={handleChange} value={formFields.email} />
+        <label htmlFor="password"></label>
+        <input type="password" name="password" placeholder="password" onChange={handleChange} value={formFields.password} />
+        <button>Log in</button>
+      </form>     
+    </div>
   )
 }
 
