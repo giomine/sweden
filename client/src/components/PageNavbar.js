447241b225dc44c05fdb5ca7495f2425da294f83
giorgia-mineo <giomineo@gmail.com>
2023-04-16 16:53:12 +0100

    Added temporary navbar links for easier building

--- client/src/components/PageNavbar.js
+++ client/src/components/PageNavbar.js
@@ -12,6 +12,14 @@ const PageNavbar = () => {
         <div>
           {/* <Link className='margin' to='/profile'>Profile</Link>
           <Link to='/'>Logout</Link> */}
+          
+          {/* TEMPORARY LINKS FOR DEBUGGING */}
+          <Link style={{ color: 'black', marginRight: '5px' }} to='/city'>Sgl</Link>
+          <Link style={{ color: 'black', marginRight: '5px' }} to='/profile'>Prof</Link>
+          <Link style={{ color: 'black', marginRight: '5px' }} to='/createcity'>CrCi</Link>
+          <Link style={{ color: 'black', marginRight: '5px' }} to='/createattraction'>CrAtt</Link>
+
+
           <Link className='margin' to='/register'>Register</Link>
           <Link to='/login'>Login</Link>
         </div>
