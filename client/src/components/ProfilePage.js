849ee66e07b7258eba5dd830d9e7fc2c586c6bc4
giorgia-mineo <giomineo@gmail.com>
2023-04-18 12:44:06 +0100

    Profile shows citycards created by logged in user

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { getToken, isAuthenticated } from '../helpers/auth'
+import Card from './Card'
+import { Link } from 'react-router-dom'
 
 const ProfilePage = () => {
 
   const [ profile, setProfile ] = useState('')
+  const [ cities, setCities ] = useState()
 
   useEffect(() => {
     const getData = async () => {
@@ -15,7 +18,26 @@ const ProfilePage = () => {
           },
         })
         setProfile(data)
-        console.log(data)
+        // console.log('Original profile -->', profile)
+        // console.log('USER ID -->', profile.id)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getData()
+  },[])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('/api/sweden/')
+        setCities(data)
+        // console.log('all cities -->', data.map(city => city))
+        // console.log('cities -->', data.map(city => city.owner.id))
+        const arr = []
+        data.map(city => city.owner.id === profile.id && arr.push(city))
+        // setCities(arr)
+        // console.log('CITIES BELONGING TO THIS USER -->', arr)
       } catch (err) {
         console.log(err)
       }
@@ -41,15 +63,41 @@ const ProfilePage = () => {
                   </div>
                 </div>
 
-                <div className='profile-section'>
-                  <div>Cards added by {profile.username}</div>
+                <div style={{ display: 'flex' }} className='profile-section'>
+                  <div style={{ margin: '0 10px' }}>Cards added by {profile.username}</div>
+                  <div style={{ margin: '0 10px' }}>Facts added by {profile.username}</div>
+                  <div style={{ margin: '0 10px' }}>Attractions added by {profile.username}</div>
                 </div>
 
-                <div className='profile-section'>
+                <div className='grid-container'>
+                  <div className='card-container'>
+                    {cities ? 
+                      cities.map(city => {
+                        if (city.owner.id === profile.id) {
+                          const { id, image, name, description } = city
+                          return (
+                            <div key={id}>
+                              <Link to={`/city/${id}`}>
+                                <Card
+                                  image={image}
+                                  name={name}
+                                  description={description}
+                                />
+                              </Link>
+                            </div>
+                          )
+                        }
+                      })
+                      : 'No cards added yet!'
+                    }
+                  </div>
+                </div>
+
+                <div  style={{ display: 'none' }} className='profile-section'>
                   <div>Facts added by {profile.username}</div>
                 </div>
 
-                <div className='profile-section'>
+                <div style={{ display: 'none' }} className='profile-section'>
                   <div>Attractions added by {profile.username}</div>
                 </div>
               </>
