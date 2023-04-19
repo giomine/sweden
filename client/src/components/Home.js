567176fa5ebbcbfc0fb4accd10ee860d5120f94c
giorgia-mineo <giomineo@gmail.com>
2023-04-19 09:08:50 +0100

    Added test map to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,37 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import mapboxgl from '!mapbox-gl'
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2lvcmdpYW1pbmVvIiwiYSI6ImNsZ205eWdiYjAzdTAzbW1tdWxrbTJzNmsifQ.PpAakCbm5jEU3_1Oq3_pdw'
 import axios from 'axios'
 import Card from './Card'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
 
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(14.66)
+  const [lat, setLat] = useState(58.63)
+  const [zoom, setZoom] = useState(4.4)
+
+  useEffect(() => {
+    if (map.current) return // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      // style: 'mapbox://styles/mapbox/streets-v12',
+      style: 'mapbox://styles/giorgiamineo/clgmck52m00ci01qybnxh2gw8',
+      center: [lng, lat],
+      zoom: zoom,
+    })
+  })
+
+  useEffect(() => {
+    if (!map.current) return // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4))
+      setLat(map.current.getCenter().lat.toFixed(4))
+      setZoom(map.current.getZoom().toFixed(2))
+    })
+  })
 
   const [ allData, setAllData ] = useState('')
 
@@ -24,6 +51,14 @@ const Home = () => {
   return (
     <div className='grid-container'>
       <div className='hero'><h1>Home</h1></div>
+
+      <div>
+        <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
+        <div ref={mapContainer} className="map-container" />
+      </div>
+
       <div className='card-container'>
         {allData.length > 0 ? 
           allData.map(data => {
