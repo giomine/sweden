1f045fd5fedd5f2ad3f14a91ce5b3535022db4b1
giorgia-mineo <giomineo@gmail.com>
2023-04-21 14:34:55 +0100

    Added map to createAttraction page + user can drop pin with dblClick

--- client/src/components/CreateAttraction.js
+++ client/src/components/CreateAttraction.js
@@ -2,17 +2,23 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { getToken } from '../helpers/auth'
 import { useNavigate } from 'react-router-dom'
+import ReactMapboxGl, { Marker, Popup } from 'react-mapbox-gl'
 
+const Map = ReactMapboxGl({ accessToken: process.env.REACT_APP_MAP_TOKEN })
 
 const CreateAttraction = () => {
 
   const navigate = useNavigate()
+  const [ lat, setLat ] = useState()
+  const [ lng, setLng ] = useState()
 
   const [ cities, setCities ] = useState('')
   const [ formFields, setFormFields ] = useState({
     name: '',
     description: '',
     city: '',
+    lat: '',
+    long: '',
     // url: '',
   })
 
@@ -24,8 +30,17 @@ const CreateAttraction = () => {
     formFields.city = e.target.value
   }
 
+  const handleDblClick = (map, event) => {
+    const lngLat = event.lngLat
+    // console.log(lngLat, lngLat.lat, lngLat.lng)
+    setLat(lngLat.lat)
+    setLng(lngLat.lng)
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault()
+    formFields.lat = lat
+    formFields.long = lng
     try {
       await axios.post('/api/attractions/', formFields, {
         headers: {
@@ -83,6 +98,25 @@ const CreateAttraction = () => {
           {/* <label htmlFor="url"></label>
           <input type="url" name="url" placeholder='Add link to website or Google Maps pin' value={formFields.url} onChange={handleChange} /> */}
 
+          Double click to drop pin
+          <Map
+            onDblClick={handleDblClick}
+            center={[14.66, 60.23]}
+            zoom={[4.3]}
+            mapboxAccessToken={process.env.REACT_APP_MAP_TOKEN}
+            style="mapbox://styles/mapbox/streets-v8"
+            containerStyle={{
+              height: '300px',
+              width: '398px',
+            }}>
+            {lng && 
+              <Marker
+                coordinates={[lng, lat]}
+                anchor="bottom">
+                <i style={{ color: 'red' }} className="fa-solid fa-map-marker"></i>
+              </Marker>
+            }
+          </Map>
 
           <button>Submit</button>
 
